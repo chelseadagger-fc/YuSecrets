fd459231d8f09d599ac51728d51cd1789b445ef0
Daniel <daniel.aldridge@alumni.unc.edu>
2023-06-15 00:58:12 +0300

    added simple (unsecure) username/password checking

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //jshint esversion:6
 const express = require("express");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
 
 const app = express();
@@ -7,18 +8,66 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+// ////////////////////////////////////
+
+main().catch(err => console.log(err));
+
+async function main() {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect('mongodb://127.0.0.1:27017/userDB');
+}
+
+const userSchema = {
+    email: String,
+    password: String
+}
+
+const User = new mongoose.model("User", userSchema);
+
+// ////////////////////////////////////
 
 app.get("/", function(req,res) {
     res.render("home");
-})
+});
 app.get("/login", function(req,res) {
     res.render("login");
-})
+});
 app.get("/register", function(req,res) {
     res.render("register");
+});
+
+app.post("/register", function(req,res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save()
+        .then(function() {
+            res.render("secrets");
+        })
+        .catch(function() {
+            console.log(error);
+        })
+    ;
 })
 
+app.post("/login", function(req,res) {
+    const username = req.body.username;
+    const password = req.body.password;
 
+    User.findOne({email: username})
+        .then((foundUser) => {
+            if (foundUser.password === password) {
+                res.render("secrets");
+            } else {
+                console.log("Username/password mismatch.");
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+})
 
 app.listen(3000, function() {
     console.log("Sever is running; listening to port 3000")
