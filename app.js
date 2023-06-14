03802abfde60812613fd1fb137fc6153541fdeda
Daniel <daniel.aldridge@alumni.unc.edu>
2023-06-14 23:36:27 +0300

    installed dependencies; set up get routes

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 //jshint esversion:6
+const express = require("express");
+const ejs = require("ejs");
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+
+app.get("/", function(req,res) {
+    res.render("home");
+})
+app.get("/login", function(req,res) {
+    res.render("login");
+})
+app.get("/register", function(req,res) {
+    res.render("register");
+})
+
+
+
+app.listen(3000, function() {
+    console.log("Sever is running; listening to port 3000")
+})
