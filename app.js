2ae51040b7ddaa207f138d8cdf8e3e63df6eab09
Daniel <daniel.aldridge@alumni.unc.edu>
2023-06-20 19:20:12 +0300

    replaced encrypted passwords with md5 hashing

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
 
 const express = require("express");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
 const ejs = require("ejs");
+const md5 = require("md5");
 require("dotenv").config();
+
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -26,7 +27,6 @@ const userSchema = new mongoose.Schema ({
 });
 
 
-userSchema.plugin(encrypt,{ secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -45,7 +45,7 @@ app.get("/register", function(req,res) {
 app.post("/register", function(req,res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save()
@@ -60,7 +60,7 @@ app.post("/register", function(req,res) {
 
 app.post("/login", function(req,res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username})
         .then((foundUser) => {
