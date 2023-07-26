14c6c1e7892f3be2084a76903dfec30929e14222
Daniel <daniel.aldridge@alumni.unc.edu>
2023-07-26 23:29:03 +0300

    installed npm modules passport, passport-local, passport-local-mongoose, express-session

--- app.js
+++ app.js
@@ -69,11 +69,13 @@ app.post("/login", function(req,res) {
 
     User.findOne({email: username})
         .then((foundUser) => {
-            if (foundUser.password === password) {
-                res.render("secrets");
-            } else {
-                console.log("Username/password mismatch.");
-            }
+            bcrypt.compare(password, foundUser.password, function(err, result) {
+                if (result === true) {
+                    result.render("secrets");
+                } else {
+                    console.log("Username/password mismatch.");
+                }
+            })
         })
         .catch((error) => {
             console.log(error);
