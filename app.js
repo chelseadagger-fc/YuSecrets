d97c13e8f8b5509a3fb318b35bc4498016a30e09
Daniel <daniel.aldridge@alumni.unc.edu>
2023-07-26 23:31:41 +0300

    removed bcrypt from /register and /login routes

--- app.js
+++ app.js
@@ -3,12 +3,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
-const bcrypt = require("bcrypt");
 require("dotenv").config();
 
 
 const app = express();
-const saltRounds = 10;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -44,42 +42,11 @@ app.get("/register", function(req,res) {
 });
 
 app.post("/register", function(req,res) {
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-    
-        newUser.save()
-            .then(function() {
-                res.render("secrets");
-            })
-            .catch(function() {
-                console.log(error);
-            })
-        ;
-    });
-
 
 })
 
 app.post("/login", function(req,res) {
-    const username = req.body.username;
-    const password = req.body.password;
 
-    User.findOne({email: username})
-        .then((foundUser) => {
-            bcrypt.compare(password, foundUser.password, function(err, result) {
-                if (result === true) {
-                    result.render("secrets");
-                } else {
-                    console.log("Username/password mismatch.");
-                }
-            })
-        })
-        .catch((error) => {
-            console.log(error);
-        })
 })
 
 app.listen(3000, function() {
